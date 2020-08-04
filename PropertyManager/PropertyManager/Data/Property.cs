5fbe159f41c651c3f017ae125714ccbe22089db8
ElliotWhiley <elliot.whiley94@gmail.com>
2020-08-04 12:05:30 +1200

    Add logic for pulling property address from trade me listing

---
+++ PropertyManager/PropertyManager/Data/Property.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertyManager.Data
+{
+    public class Property
+    {
+        public string Address { get; set; }
+        public decimal Cv { get; set; }
+    }
+}
---
+++ PropertyManager/PropertyManager/Data/WebScraperService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace PropertyManager.Data
+{
+    public class WebScraperService
+    {
+        const string TradeMeAddressXpath = "/html/body/trade-me/div[1]/main/div/tm-property-listing/div/tm-property-listing-body/div/section[1]/tg-row/tg-col/h1";
+
+        public async Task<Property> GetPropertyAsync(string propertyUrl)
+        {
+            var web = new HtmlAgilityPack.HtmlWeb();
+            var document = await web.LoadFromWebAsync(propertyUrl);
+            var address = document.DocumentNode.SelectSingleNode(TradeMeAddressXpath).InnerText;
+
+            return new Property {
+                Address = address,
+                Cv = 900000
+            };
+        }
+    }
+}
