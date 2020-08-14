2442138f57ad730a5f46eccfd034c1c1c8367429
ElliotWhiley <elliot.whiley94@gmail.com>
2020-08-14 17:10:27 +1200

    Improve using statement

--- PropertyManager/PropertyManager/Data/StringExtensions.cs
+++ PropertyManager/PropertyManager/Data/StringExtensions.cs
@@ -1,9 +1,11 @@
-﻿namespace PropertyManager.Data {
+﻿using System;
+
+namespace PropertyManager.Data {
     public static class StringExtensions {
         public static string Base64Encode(this string text)
         {
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(text);
-            return System.Convert.ToBase64String(plainTextBytes);
+            return Convert.ToBase64String(plainTextBytes);
         }
     }
 }
